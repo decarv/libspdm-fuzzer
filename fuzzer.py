65f6ac2ba8a7b389172bf03be8f1cde751ab11d7
Henrique de Carvalho <decarv@pop-os.localdomain>
2023-02-22 17:30:34 -0300

    Add fuzzer.py

---
+++ fuzzer.py
@@ -0,0 +1,17 @@
+""" Fuzzer for the SPDM
+
+Copyright (C) Henrique de Carvalho <henriquecarvalho@usp.br | decarv.henrique@gmail.com>
+"""
+
+class FuzzingEngine:
+    pass
+
+class GenerationEngine:
+    pass
+
+def main():
+    print("main not implemented")
+    # TODO
+
+if __name__ == "__main__":
+    main()
